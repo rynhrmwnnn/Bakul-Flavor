7d0cf1e84edb70da395a90200850ae7e566ac18e
rynhrmwnnn <hermawanryan626@gmail.com>
2023-11-14 21:27:31 +0700

    menambahkan file

---
+++ navbar.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand navbar-dark bg-primary sticky-top">
+      <div class="container-lg">
+        <a class="navbar-brand" href=".">Bakul Flavor</a>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarNavDropdown"
+        >
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Username
+              </a>
+              <ul class="dropdown-menu dropdown-menu-end mt-3">
+                <li>
+                  <a class="dropdown-item" href="#"
+                    ><i class="bi bi-person-fill"></i> Profil</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#"
+                    ><i class="bi bi-key"></i> Change Password</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    <i class="bi bi-box-arrow-right"></i> Log Out</a
+                  >
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
---
+++ report.php
@@ -0,0 +1,14 @@
+<div class="col-lg-9 mt-2">
+  <div class="card">
+    <div class="card-header">Report</div>
+    <div class="card-body">
+      <h5 class="card-title">Bentar Sabar Dulu</h5>
+      <p class="card-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Reprehenderit, esse impedit modi exercitationem officia maiores
+        natus! Quaerat fugit pariatur dolor.
+      </p>
+      <a href="#" class="btn btn-primary">Klik</a>
+    </div>
+  </div>
+</div>
